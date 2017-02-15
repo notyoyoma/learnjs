da3d24a5df0b2b506d9982e3652a426c3a846d0f
mnaselli <marty.naselli@gmail.com>
2017-02-15 17:19:12 -0500

    did stuff

--- gulp_files/gulp-pug.js
+++ gulp_files/gulp-pug.js
@@ -2,12 +2,22 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 var humanize = require('string-humanize');
 
+var glob = require('glob');
+
+var pages = [];
+glob('app/!(_|index)*.pug', {}, function(er, files){
+    for (var i=0; i<files.length; i++) {
+        pages.push(files[i].match(/^app\/([^.]*).pug/)[1]);
+    }
+});
+
 gulp.task('pug', function() {
 	return gulp.src('app/**/!(_)*.pug')
 		.pipe(pug({
 			pretty: true,
             locals: {
-                'humanize': humanize
+                'humanize': humanize,
+                'pages': pages
             }
 		}))
 		.pipe(gulp.dest('./dist/'));
